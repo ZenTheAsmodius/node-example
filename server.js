9326ce1d2d69e6ea1a9238218972ed6ca4703737
Stefan Jovic <stefanjovic32@gmail.com>
2023-06-14 00:25:55 +0200

    adding strict mode

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes');
@@ -12,8 +13,8 @@ app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', _req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Expose-Headers', 'set-cookies')
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Expose-Headers', 'set-cookies');
   next();
 });
 
