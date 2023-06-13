e25d14851e2f2b98adfcef909eab6cec03bcd95b
Stefan Jovic <stefanjovic32@gmail.com>
2023-06-14 00:19:52 +0200

    init

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+router.route('/').get((_req, res) => res.status(200).json({
+  message: 'Welcome!'
+}))
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', _req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Expose-Headers', 'set-cookies')
+  next();
+});
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
